30c6c24586175b14f93b13a738b779ec19b87451
SAM OCHU <109700174+SamSpeedX@users.noreply.github.com>
2024-11-10 19:28:22 +0300

    Update index.php

--- public/index.php
+++ public/index.php
@@ -2,11 +2,12 @@
 require_once __DIR__ . '/../vendor/autoload.php';
 
 use Kibalanga\Core\Router;
-use Kibalanga\App\Controllers\HomeController;
+use Kibalanga\App\Controllers\WelcomeController;
 
 $router = new Router();
 
-$router->add('GET', '/', [new HomeController(), 'index']);
-$router->add('GET', '/about', [new HomeController(), 'about']);
+// Route for the home page
+$router->add('GET', '/', [new WelcomeController(), 'index']);
 
+// Handle requests
 $router->handle($_SERVER['REQUEST_URI'], $_SERVER['REQUEST_METHOD']);
