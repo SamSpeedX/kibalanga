c03068b3561ebed8aa55bf3557d876ae149d99ef
SAM OCHU <109700174+SamSpeedX@users.noreply.github.com>
2024-11-10 19:39:58 +0300

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,9 +1,23 @@
 <?php
+
 return [
+    'app' => [
+        'name' => getenv('APP_NAME'),
+        'env' => getenv('APP_ENV'),
+        'debug' => getenv('APP_DEBUG'),
+        'url' => getenv('APP_URL'),
+    ],
     'db' => [
-        'host' => 'localhost',
-        'username' => 'root',
-        'password' => '',
-        'database' => 'kibalanga_db',
+        'host' => getenv('DB_HOST'),
+        'port' => getenv('DB_PORT'),
+        'database' => getenv('DB_DATABASE'),
+        'username' => getenv('DB_USERNAME'),
+        'password' => getenv('DB_PASSWORD'),
+    ],
+    'cache' => [
+        'driver' => getenv('CACHE_DRIVER'),
+    ],
+    'session' => [
+        'driver' => getenv('SESSION_DRIVER'),
     ],
 ];
